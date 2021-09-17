4caddcb6fae4153c67ee82d30ba00b5e29bfee27
Diego Feversani <diego.feversani@gmail.com>
2021-09-17 15:41:02 -0300

    Se agrego las carpetas de Jenkins y los scripts

--- jenkins/scripts/script.sh
+++ jenkins/scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo Instalado las dependencia del proyecto
-npm i
+#echo Instalado las dependencia del proyecto
+#npm i
 
-echo Contruyendo el build del proyecto
-npm run build
+#echo Contruyendo el build del proyecto
+#npm run build
 
 echo Creando imagen del proyecto
 docker-compose build
